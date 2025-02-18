a46a606334709b78849d83afe4c3b456fa70a3bf
AABB2741 <mariosantos88477364484@gmail.com>
2025-02-18 09:35:06 -0300

    feat: Aula 2

--- apps/web/src/components/button.tsx
+++ apps/web/src/components/button.tsx
@@ -1,21 +1,25 @@
 import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
-export function Button({ ...rest }: ButtonProps) {
+export function Button({ ...props }: ButtonProps) {
   return (
     <button
       className="flex justify-between items-center font-semibold rounded-xl px-5 h-12 bg-gray-500 text-blue w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
-      {...rest}
+      {...props}
     />
   );
 }
 
-export function IconButton({ ...rest }: ButtonProps) {
+export function IconButton({ className, ...props }: ButtonProps) {
   return (
     <button
-      className="rounded-md p-1.5 h-12 bg-gray-500 text-blue cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
-      {...rest}
+      className={twMerge(
+        "rounded-md p-1.5 bg-gray-500 text-blue cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300",
+        className
+      )}
+      {...props}
     />
   );
 }
